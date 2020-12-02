a6bd242641d114b7b3e26e8694ce3c4aec34af50
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:51:41 +0300

    Create 11.11.2020-4.java

---
+++ 11.11.2020-4.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class dfg {
+    public class Program{
+
+        public static void main(String[] args) {
+
+            Person undef = new Person();
+            undef.displayInfo();
+
+            Person tom = new Person("Tom");
+            tom.displayInfo();
+
+            Person sam = new Person("Sam", 25);
+            sam.displayInfo();
+        }
+    }
+    class Person{
+
+        String name;    // имя
+        int age;        // возраст
+        Person()
+        {
+            this("Undefined", 18);
+        }
+        Person(String name)
+        {
+            this(name, 18);
+        }
+        Person(String name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        void displayInfo(){
+            System.out.printf("Name: %s \tAge: %d\n", name, age);
+        }
+    }
+
+}
